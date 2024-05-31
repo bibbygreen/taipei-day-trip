c43a61e581d67cfb9ea206b90c0af1c61022ff92
bibbygreen <a20034425@gmail.com>
2024-05-31 20:18:54 +0800

    modify mysql connection-2

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from fastapi import *
 from fastapi.responses import FileResponse
 from fastapi.responses import JSONResponse
 import mysql.connector
+import socket
+import ssl
+
+hostname = 'www.python.org'
+context = ssl.create_default_context()
+
+with socket.create_connection((hostname, 443)) as sock:
+    with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+        print(ssock.version())
 app=FastAPI()
 
 # 連線資料庫
