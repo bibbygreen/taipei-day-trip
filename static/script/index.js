2d6f38a35cb56b6f9b40f14244201cb57cb222cb
bibbygreen <a20034425@gmail.com>
2024-06-08 21:22:33 +0800

    finish part2

--- static/script/index.js
+++ static/script/index.js
@@ -1,6 +1,17 @@
+const searchButton = document.getElementById("search-button");
+const searchQueryText=document.getElementById("search-query-text");
+const attractionsContainer = document.querySelector(".attractions-container");
+const loadMoreTrigger = document.getElementById('load-more-trigger');
 
-let mrt_list_container=document.querySelector(".mrt-list-container");
-let mrt_list_block=document.querySelector(".mrt-list-block");
+let nextPage = 0;  // Start with the initial page
+let isFetching = false;  // To prevent multiple fetches
+let currentKeyword = "";  // Track the current keyword
+// Event listener
+searchButton.addEventListener("click", queryBySearchBar);
+
+
+let mrtListContainer=document.querySelector(".mrt-list-container");
+let mrtListBlock=document.querySelector(".mrt-list-block");
 // 取得捷運站列表
 // let url="/api/mrts";
 // let url="http://52.89.165.57:8000/api/mrts";
@@ -14,190 +25,232 @@ fetch(url)
   })
   .then((data) => {
     let mrts = data["data"];
-    for (mrt of mrts) {
-        let mrt_list_item = document.createElement("div");
-        mrt_list_item.textContent = mrt;
-        mrt_list_item.className = "mrt-list-text";
-        // mrt_list_item.addEventListener("click", searchByMRT);
-        mrt_list_container.appendChild(mrt_list_item);
+    for (let mrt of mrts) {
+        let mrtListItem = document.createElement("div");
+        mrtListItem.textContent = mrt;
+        mrtListItem.className = "mrt-list-text";
+        // mrtListItem.addEventListener("click", searchByMRT);
+        mrtListItem.addEventListener("click", function(event) {
+          queryByClickMrt(event.target);
+        });
+        mrtListContainer.appendChild(mrtListItem);
     }
   })
   .catch((error) => {
     console.error('There has been a problem with your fetch operation:', error);
   });
 
-
 // mrt scroll bar
 let btn_left = document.getElementById("btn-left");
 let btn_right = document.getElementById("btn-right");
+// Function to calculate scrollAmount based on the width of the container
+function calculateScrollAmount() {
+  // Get the width of the container
+  const containerWidth = mrtListContainer.offsetWidth;
+  return containerWidth/2; 
+}
+// Function to handle scrolling to the left
+function scrollLeft() {
+  mrtListContainer.scrollBy({
+    left: -calculateScrollAmount(),
+    behavior: 'smooth' // Use smooth scrolling behavior
+  });
+}
+// Function to handle scrolling to the right
+function scrollRight() {
+  mrtListContainer.scrollBy({
+    left: calculateScrollAmount(),
+    behavior: 'smooth' // Use smooth scrolling behavior
+  });
+}
+btn_left.addEventListener("click", scrollLeft);
+btn_right.addEventListener("click", scrollRight);
 
-// btn_left.addEventListener("click", scrollLeft);
-// btn_right.addEventListener("click", scrollRight);
+// Attach click event listener to the MRT Station names
+mrtListBlock.addEventListener("click", function(event) {
+  // Check if the click occurred on an MRT Station name
+  if (event.target.classList.contains("mrt-list-text")) {
+    queryByClickMrt(event.target);
+  }
+});
+// function start ----------
+function fetchAndRenderAttractions(page, keyword="") {
+  let url = `http://127.0.0.1:8000/api/attractions?page=${page}`;
+  // url = `/api/attractions?page=${page}`;
+  if(keyword) {
+    url+=`&keyword=${keyword}`;
+  }
+  url=encodeURI(url);
+  isFetching=true;  // Set fetching flag to true
 
-// url="http://127.0.0.1:8000/api/attractions?page=0"
-// fetch(url)
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok ' + response.statusText);
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     let attractions=data["data"];
-//     let nextPage=data["nextPage"];
-//     renderAttractions(attractions);
-    
-// })
-
-const attractions_container = document.querySelector(".attractions-container");
-const loadMoreTrigger = document.getElementById('load-more-trigger');
-let nextPage = 0;  // Start with the initial page
-let isFetching = false;  // To prevent multiple fetches
-
-function fetchAndRenderAttractions(page) {
-    const url = `http://127.0.0.1:8000/api/attractions?page=${page}`;
-    isFetching = true;  // Set fetching flag to true
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        renderAttractions(data.data);
-        nextPage = data.nextPage;
-        isFetching = false;  // Reset fetching flag
-      })
-      .catch((error) => {
-        console.error('There has been a problem with your fetch operation:', error);
-        isFetching = false;  // Reset fetching flag on error
-      });
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (page === 0) {
+        // First page: replace existing list
+        attractionsContainer.replaceChildren();
+      }
+      renderAttractions(data.data);
+      nextPage=data.nextPage;
+      isFetching=false;  // Reset fetching flag
+    })
+    .catch((error) => {
+      console.error('There has been a problem with your fetch operation:', error);
+      isFetching = false;  // Reset fetching flag on error
+    });
   }
 
-// function renderAttractions(attractions){
-//     for (let attraction of attractions){
-//     const attraction_block = document.createElement("div");
-//     attraction_block.className="attraction-block";
-//     attractions_container.appendChild(attraction_block);
-
-//     const attraction_img=document.createElement("div");
-//     attraction_img.className="attraction-img";
-//     first_image_url=attraction["images"][0];
-//     attraction_img.style.backgroundImage=`url(${first_image_url})`;
-    
-//     const attraction_name=document.createElement("div");
-//     attraction_name.className="attraction-name"
-
-//     const attraction_name_txt=document.createElement("div");
-//     attraction_name_txt.className="attraction-name-txt";
-//     attraction_name_txt.textContent=`${attraction["name"]}`;
-    
-//     const attraction_info=document.createElement("div");
-//     attraction_info.className="attraction-info";
-//     const attraction_info_mrt=document.createElement("div");
-//     attraction_info_mrt.className="attraction-info-mrt";
-//     attraction_info_mrt.textContent=`${attraction["mrt"]}`;
-//     const attraction_info_category=document.createElement("div");
-//     attraction_info_category.className="attraction-info-category";
-//     attraction_info_category.textContent=`${attraction["category"]}`;
-//     attraction_block.appendChild(attraction_img);
-//     attraction_name.appendChild(attraction_name_txt);
-//     attraction_block.appendChild(attraction_name);
-//     attraction_info.appendChild(attraction_info_mrt);
-//     attraction_info.appendChild(attraction_info_category);
-//     attraction_block.appendChild(attraction_info);
-//     }
-// }   
-
 function renderAttractions(attractions) {
-    attractions.forEach(attraction => {
-        const attractionBlock = createAttractionBlock(attraction);
-        attractions_container.appendChild(attractionBlock);
-    });
+  attractions.forEach(attraction => {
+      const attractionBlock = createAttractionBlock(attraction);
+      attractionsContainer.appendChild(attractionBlock);
+  });
 }
 
 function createAttractionBlock(attraction) {
-    const attractionBlock = document.createElement("div");
-    attractionBlock.className = "attraction-block";
+  const attractionBlock = document.createElement("div");
+  attractionBlock.className = "attraction-block";
 
-    const attractionImg = createAttractionImage(attraction.images);
-    attractionBlock.appendChild(attractionImg);
+  const attractionImg = createAttractionImage(attraction.images);
+  attractionBlock.appendChild(attractionImg);
 
-    const attractionName = createAttractionName(attraction.name);
-    attractionBlock.appendChild(attractionName);
+  const attractionName = createAttractionName(attraction.name);
+  attractionBlock.appendChild(attractionName);
 
-    const attractionInfo = createAttractionInfo(attraction.mrt, attraction.category);
-    attractionBlock.appendChild(attractionInfo);
+  const attractionInfo = createAttractionInfo(attraction.mrt, attraction.category);
+  attractionBlock.appendChild(attractionInfo);
 
-    return attractionBlock;
+  return attractionBlock;
 }
 
 function createAttractionImage(images) {
-    const attractionImg = document.createElement("div");
-    attractionImg.className = "attraction-img";
-    
-    // Handle missing or empty images array
-    const firstImageUrl = images && images.length > 0 ? images[0] : 'default-image-url.jpg';
-    attractionImg.style.backgroundImage = `url(${firstImageUrl})`;
+  const attractionImg = document.createElement("div");
+  attractionImg.className = "attraction-img";
+  
+  // Handle missing or empty images array
+  const firstImageUrl = images && images.length > 0 ? images[0] : 'default-image-url.jpg';
+  attractionImg.style.backgroundImage = `url(${firstImageUrl})`;
 
-    return attractionImg;
+  return attractionImg;
 }
 
 function createAttractionName(name) {
-    const attractionName = document.createElement("div");
-    attractionName.className = "attraction-name";
+  const attractionName = document.createElement("div");
+  attractionName.className = "attraction-name";
 
-    const attractionNameTxt = document.createElement("div");
-    attractionNameTxt.className = "attraction-name-txt";
-    attractionNameTxt.textContent = name || 'Unknown Name';
+  const attractionNameTxt = document.createElement("div");
+  attractionNameTxt.className = "attraction-name-txt";
+  attractionNameTxt.textContent = name || 'Unknown';
 
-    attractionName.appendChild(attractionNameTxt);
-
-    return attractionName;
+  attractionName.appendChild(attractionNameTxt);
+  return attractionName;
 }
 
 function createAttractionInfo(mrt, category) {
-    const attractionInfo = document.createElement("div");
-    attractionInfo.className = "attraction-info";
+  const attractionInfo = document.createElement("div");
+  attractionInfo.className = "attraction-info";
 
-    const attractionInfoMrt = document.createElement("div");
-    attractionInfoMrt.className = "attraction-info-mrt";
-    attractionInfoMrt.textContent = mrt || '無';
+  const attractionInfoMrt = document.createElement("div");
+  attractionInfoMrt.className = "attraction-info-mrt";
+  attractionInfoMrt.textContent = mrt || '無';
 
-    const attractionInfoCategory = document.createElement("div");
-    attractionInfoCategory.className = "attraction-info-category";
-    attractionInfoCategory.textContent = category || 'Unknown';
+  const attractionInfoCategory = document.createElement("div");
+  attractionInfoCategory.className = "attraction-info-category";
+  attractionInfoCategory.textContent = category || 'Unknown';
 
-    attractionInfo.appendChild(attractionInfoMrt);
-    attractionInfo.appendChild(attractionInfoCategory);
+  attractionInfo.appendChild(attractionInfoMrt);
+  attractionInfo.appendChild(attractionInfoCategory);
 
-    return attractionInfo;
+  return attractionInfo;
+}
+// query by search bar
+// function queryBySearchBar() {
+//   currentKeyword = searchQueryText.value;  // Update the current keyword
+//   nextPage = 0;  // Reset nextPage to 0 for a new search
+//   fetchAndRenderAttractions(nextPage, currentKeyword);
+//   searchQueryText.value = "";
+// }
+function queryBySearchBar(){
+  currentKeyword = searchQueryText.value;  // Update the current keyword
+  nextPage = 0;  // Reset nextPage to 0 for a new search
+  // url = `/api/attractions?page=0&keyword=${searchQueryText.value}`;
+  url = `http://127.0.0.1:8000/api/attractions?page=0&keyword=${currentKeyword}`;
+  url = encodeURI(url);
+  fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    attractions = data["data"];
+    nextPage = data["nextPage"];
+    attractionsContainer.replaceChildren();  // Replace existing list
+    renderAttractions(attractions);
+    // if (nextPage != null){
+    //   fetchAndRenderAttractions(nextPage, currentKeyword);
+    // }
+    // currentKeyword="";
+});
 }
 
-// IntersectionObserver callback
-function loadMoreEntries(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting && !isFetching && nextPage !== null) {
-            fetchAndRenderAttractions(nextPage);
-            }
-    });
+function queryByClickMrt(target){
+  const clickedMRTStation = target.textContent.trim();
+  searchQueryText.value = clickedMRTStation;
+  queryBySearchBar();
 }
-  
-  // Set up IntersectionObserver
-function setupIntersectionObserver() {
-    const observer = new IntersectionObserver(loadMoreEntries, {
-        root: null,  // Default is the viewport
-        rootMargin: '0px',
-        threshold: 1.0
-    });
-    
-    observer.observe(loadMoreTrigger);
-    return observer; // Return the observer
+// Function to handle window scroll event
+function handleScroll() {
+  // Check if the user has scrolled to the bottom of the page
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    // If not currently fetching and there is a next page, fetch and render attractions
+    if (!isFetching && nextPage !== null) {
+      fetchAndRenderAttractions(nextPage, currentKeyword);
+    }
+  }
 }
-  
+
+// Attach scroll event listener to the window
+window.addEventListener('scroll', handleScroll);
+
 // Initial fetch when DOM content is loaded
 document.addEventListener("DOMContentLoaded", () => {
-    fetchAndRenderAttractions(nextPage);
-    setupIntersectionObserver();
-  });
+  fetchAndRenderAttractions(nextPage, currentKeyword);
+});
+// function renderNextPage(entries, observer) {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting && !isFetching && nextPage !== null) {
+//       fetchAndRenderAttractions(nextPage, currentKeyword);
+//     }
+//   });
+// }
+// IntersectionObserver callback
+// function loadMoreEntries(entries, observer) {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting && !isFetching && nextPage !== null) {
+//       fetchAndRenderAttractions(nextPage, currentKeyword);
+//       }
+//   });
+// }
+// // Set up IntersectionObserver
+// function setupIntersectionObserver() {
+//   const observer = new IntersectionObserver(loadMoreEntries, {
+//       root: null,  // Default is the viewport
+//       rootMargin: '0px',
+//       threshold: 1.0
+//   });   
+//   observer.observe(loadMoreTrigger);
+//   return observer; // Return the observer
+// }
+  
+// // Initial fetch when DOM content is loaded
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetchAndRenderAttractions(nextPage);
+//   setupIntersectionObserver();
+// });
